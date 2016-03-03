7b20620556159f35f6091cbfb5c19f2a88c2881c
travy3 <zyu.lolee@gmail.com>
2016-03-03 22:27:45 +0800

    *shiro

--- src/main/java/com/zyu/xjsy/modules/sys/security/FormAuthenticationFilter.java
+++ src/main/java/com/zyu/xjsy/modules/sys/security/FormAuthenticationFilter.java
@@ -1,23 +1,37 @@
 package com.zyu.xjsy.modules.sys.security;
 
+import com.zyu.xjsy.modules.sys.util.UserUtils;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.util.WebUtils;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by chenjie on 2016/3/3.
  */
 public class FormAuthenticationFilter extends org.apache.shiro.web.filter.authc.FormAuthenticationFilter {
 
+    private static final String DEFAULT_CAPTCHA_PARAM = "validateCode";
 
     @Override
     protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) {
 
         String name = getUsername(request);
         String password = getPassword(request);
+        boolean remeberMe = isRememberMe(request);
 
 
-        return super.createToken(request, response);
+        String captcha = getCaptcha(request);
+
+        String host = UserUtils.getRemtoAddr((HttpServletRequest) request);
+
+
+        return new  UsernamePasswordToken(name,password,remeberMe,host,captcha);
+    }
+
+    private String getCaptcha(ServletRequest request) {
+        return WebUtils.getCleanParam(request,DEFAULT_CAPTCHA_PARAM);
     }
 }
--- src/main/java/com/zyu/xjsy/modules/sys/util/UserUtils.java
+++ src/main/java/com/zyu/xjsy/modules/sys/util/UserUtils.java
@@ -1,7 +1,26 @@
 package com.zyu.xjsy.modules.sys.util;
 
+import javax.servlet.http.HttpServletRequest;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 /**
  * Created by chenjie on 2016/3/3.
  */
 public class UserUtils {
+
+    /**
+     * 获得用户远程地址
+     */
+    public static String getRemtoAddr(HttpServletRequest request){
+        String remoteAddr = request.getHeader("X-Real-IP");
+        if (isNotBlank(remoteAddr)) {
+            remoteAddr = request.getHeader("X-Forwarded-For");
+        }else if (isNotBlank(remoteAddr)) {
+            remoteAddr = request.getHeader("Proxy-Client-IP");
+        }else if (isNotBlank(remoteAddr)) {
+            remoteAddr = request.getHeader("WL-Proxy-Client-IP");
+        }
+        return remoteAddr != null ? remoteAddr : request.getRemoteAddr();
+    }
 }
